c10fbfc93876f93d5a309e110444db6bcfb1f394
EmereArco <emere.arco@gmail.com>
2016-07-25 16:54:46 +0200

    Removed hidden unused files

--- .profile.d/java_osmosis.sh
+++
@@ -1,5 +0,0 @@
-export PATH="/app/.apt/usr/bin/:$HOME/.apt/usr/bin:/app/.apt/usr/lib/jvm/java-7-openjdk-amd64/bin:$PATH"
-export JAVA_HOME="/app/.apt/usr/lib/jvm/java-7-openjdk-amd64"
-cp /home/vcap/app/cf/plexus.conf /app/.apt/etc/osmosis/
-cp /home/vcap/app/cf/osmosis /app/.apt/usr/bin/
-chmod 700 /app/.apt/usr/bin/osmosis
