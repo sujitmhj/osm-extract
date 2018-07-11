c687e53fe6809bf9ab79e38b38a47d38c901ff69
Ashish Acharya <ashish.acharya14@gmail.com>
2018-07-11 11:56:36 +0545

    customized the sh file for nepal

--- osm_update.sh
+++ osm_update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export PATH=$PATH:/opt/osmosis/bin
-cd /home/ubuntu/osm-extract
+NAME=nepal
+
+cd /home/geosolutions/nepal_osm_extract
 echo $(date)
-make all NAME=malawi URL="http://download.openstreetmap.fr/extracts/africa/malawi-latest.osm.pbf" && psql -d geonode -f set_pub_date.sql
+make all NAME=malawi URL="http://download.geofabrik.de/asia/nepal-latest.osm.pbf" && psql -d nepal_geonode -f set_pub_date.sql
 echo $(date)
+rm -rf $NAME/*.pbf
+rm -rf $NAME/*.sql
