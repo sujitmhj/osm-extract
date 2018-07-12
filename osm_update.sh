474f9d60dc7746363eebed75583acc86d912a5b5
Ashish Acharya <ashish.acharya14@gmail.com>
2018-07-12 12:11:19 +0545

    fixed a typo

--- osm_update.sh
+++ osm_update.sh
@@ -4,7 +4,7 @@ NAME=nepal
 
 cd /home/geosolutions/nepal_osm_extract
 echo $(date)
-make all NAME=malawi URL="http://download.geofabrik.de/asia/nepal-latest.osm.pbf" && psql -d nepal_geonode -f set_pub_date.sql
+make all NAME=$NAME URL="http://download.geofabrik.de/asia/nepal-latest.osm.pbf" && psql -d nepal_geonode -f set_pub_date.sql
 echo $(date)
 rm -rf $NAME/*.pbf
 rm -rf $NAME/*.sql
