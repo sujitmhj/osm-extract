fb0f07bbe139cac372e96bf75bbf7782132b5e35
Sujit Maharjan <sujitmhj@gmail.com>
2018-08-11 13:15:36 +0545

    update date update script to take timestamp

--- set_pub_date.sql
+++ set_pub_date.sql
@@ -1,1 +1,1 @@
-update base_resourcebase set date = current_date where id in (select resourcebase_ptr_id from layers_layer where store = 'nepal_osm');
+update base_resourcebase set date = current_timestamp where id in (select resourcebase_ptr_id from layers_layer where store = 'nepal_osm');
